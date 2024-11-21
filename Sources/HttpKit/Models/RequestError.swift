91e01560f16f25740506d7e0d5faa8f8bcd98ee7
Kemal Türk <kmltrk07@gmail.com>
2024-11-21 14:21:36 +0300

    add: public initializer

--- Sources/HttpKit/Models/RequestError.swift
+++ Sources/HttpKit/Models/RequestError.swift
@@ -1,41 +1,46 @@
 //
 //  RequestError.swift
-//  
+//
 //
 //  Created by Kemal Türk on 28.07.2022.
 //
 
 import Foundation
 
+
 public struct RequestError: Error {
-  public let statusCode: Int
-  public let body: String
-  
-  public static var invalidURL: RequestError {
-    .init(statusCode: -1, body: "Invalid Url")
-  }
-  
-  public static var noData: RequestError {
-    .init(statusCode: -1, body: "No Data")
-  }
-  
-  public static var unknown: RequestError {
-    .init(statusCode: -1, body: "Unknown Error")
-  }
-  
-  public static var noResponse: RequestError {
-    .init(statusCode: -1, body: "No Response")
-  }
-  
-  public static var decode: RequestError {
-    .init(statusCode: -1, body: "Decode Error")
-  }
-  
-  public static func unknown(_ err: Error) -> RequestError {
-    .init(statusCode: -1, body: err.localizedDescription)
-  }
-  
-  public var message: String {
-    return body
-  }
+    public let statusCode: Int
+    public let body: String
+    public init(statusCode: Int, body: String) {
+        self.statusCode = statusCode
+        self.body = body
+    }
+    
+    public static var invalidURL: RequestError {
+        .init(statusCode: -1, body: "Invalid Url")
+    }
+    
+    public static var noData: RequestError {
+        .init(statusCode: -1, body: "No Data")
+    }
+    
+    public static var unknown: RequestError {
+        .init(statusCode: -1, body: "Unknown Error")
+    }
+    
+    public static var noResponse: RequestError {
+        .init(statusCode: -1, body: "No Response")
+    }
+    
+    public static var decode: RequestError {
+        .init(statusCode: -1, body: "Decode Error")
+    }
+    
+    public static func unknown(_ err: Error) -> RequestError {
+        .init(statusCode: -1, body: err.localizedDescription)
+    }
+    
+    public var message: String {
+        return body
+    }
 }
